514b55e0b751f13ec421e9caec705ed3efc9d8a0
thanhhong <64731516+nongthanhhong@users.noreply.github.com>
2023-09-16 01:00:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ saved_checkpoint_path = "ckpt/gpu_train/transformer_AdamW_epoch_7_loss_9.1570_BL
       # saved_checkpoint_path = "ckpt/tpu_train/transformer_AdamW_epoch_7_loss_9.1570_BLEU_0.0_m9_d9_3h_54m.pt"
 
 learning_rate = 1e-4
-batch_size = 64
+batch_size = 32
 max_len_input = 50
 num_heads = 6
 num_layers = 4
